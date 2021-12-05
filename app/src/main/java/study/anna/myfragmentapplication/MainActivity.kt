ca0c81cd4b51823a031cb0194f9cf265ee94e790
annaplusnina <9827074@gmail.com>
2021-12-05 20:22:42 +0300

    first commit

---
+++ app/src/main/java/study/anna/myfragmentapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package study.anna.myfragmentapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottonNavigationMenu: BottomNavigationView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottonNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+
+        bottonNavigationMenu.setOnItemSelectedListener { item ->
+
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.fragment_1 -> {
+                    fragment = FirstFragment()
+
+                }
+                R.id.fragment_2 -> {
+                    fragment = SecondFragment()
+
+                }
+
+            }
+            replaceFragment(fragment!!)
+
+            true
+
+        }
+
+        fun replaceFragment(fragment: Fragment) {
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_Container, fragment)
+                .addToBackStack(fragment.tag)
+                .commit()
+
+        }
+
+    }
+}
